e231de033ef8b42c5b7402ad5057817d08f0352e
Miguel Á. Sastre <sastre113@gmail.com>
2024-04-05 22:27:19 +0200

    finalizado

--- exercise/littleChild/littleChield.js
+++ exercise/littleChild/littleChield.js
@@ -1,6 +1,22 @@
 
-export const littleChild = (child1, child2) => {
+/*
+   Given are two variables child1 and child2 with a numeric value between 0 and 99. 
+   Return true if a child is between 0 and 14 (0 and 14 inclusive). 
+   Return false if both or none in between. 
+*/
+
+export const littleChild = (child1, child2) => { 
     if(typeof child1 !== "number" || typeof child2 !== "number"){
-        throw new Error();
+        throw new Error('parameters provided must be numbers');
     }
+
+    if(Number.isNaN(child1) || Number.isNaN(child2)){
+        throw new Error('parameters provided must be numbers');
+    }
+
+    return isInRange(child1) ^ isInRange(child2) ? true : false;
 };
+
+const isInRange = (number) => {
+    return number >= 0 && number <= 14 
+}
--- exercise/littleChild/test/littleChield.test.js
+++ exercise/littleChild/test/littleChield.test.js
@@ -12,12 +12,35 @@ describe("Test sobre littleChield", () =>{
     });
 
     test('should throw if not number is provided as parameter', () => {
-        expect(() => littleChild()).toThrow();
+        expect(() => littleChild()).toThrow('parameters provided must be numbers');
     });
     
-    test('', () => {
-
+    test('should throw if not number is provided as parameter', () => {
+        expect(() => littleChild(NaN,NaN)).toThrow('parameters provided must be numbers');
     });
 
+    test('if child1 = 1 and child2 = 2, should return false', () => {
+        expect(littleChild(1, 2)).toBe(false);
+    });
+    
+    test('if child1 = 15 and child2 = -1, should return false', () => {
+        expect(littleChild(15, -1)).toBe(false);
+    });
+    
+    test('if child1 = 0 and child2 = 3, should return false', () => {
+        expect(littleChild(0, 3)).toBe(false);
+    });
+
+    test('if child1 = -1 and child2 = 13, should return true', () => {
+        expect(littleChild(-1, 13)).toBe(true);
+    });
+
+    test('if child1 = 5 and child2 = -1, should return true', () => {
+        expect(littleChild(5, -1)).toBe(true);
+    });
+
+    test('if child1 = 0 and child2 = 14, should return false', () => {
+        expect(littleChild(0, 14)).toBe(false);
+    });
 
 });
